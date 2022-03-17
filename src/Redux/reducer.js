a205f6749440f738dac2f08008b6e000f2bad8df
ANURUDDH <hellomoto.1769@gmail.com>
2022-03-17 16:56:54 +0530

    psh

---
+++ src/Redux/reducer.js
@@ -0,0 +1,12 @@
+
+
+const initState = {
+    data:[],
+    filterData:[],
+    isLoading:false,
+    isError:false
+}
+
+export const Reducer = ()=>{
+    // add the switch statement for different actions
+}
